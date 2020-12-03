500661104902a13b605892ae831dcd509fde0675
liuranran <864749024@qq.com>
2020-12-03 13:50:16 +0800

    feat:add basic style and loader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+  components: {}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
+
+import ElementUI from 'element-ui'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+import './styles/element-variables.scss'
+
 import App from './App.vue'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
